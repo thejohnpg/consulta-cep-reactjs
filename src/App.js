835273dafb31ac4e7be2dc9de8e9e6287b9583ff
John Peterson <thejohnpg@gmail.com>
2020-03-24 22:00:47 -0300

    chore: add link and icons to repository and linkedin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 import Consulta from './components/Consulta'
 
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <a href="https://github.com/thejohnpg/consulta-cep-reactjs/" target="_blank" className="header-icon"><FaGithub /></a> 
+        <a href="https://www.linkedin.com/in/thejohnpg/" target="_blank" className="header-icon" ><FaLinkedin /></a> 
+      </div>
      <Consulta />
     </div>
   );
