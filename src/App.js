c8bce8ec2bc022442e1cf1b14402af178239c3c6
John Peterson <thejohnpg@gmail.com>
2020-03-31 22:37:08 -0300

    fix: adjustments css

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ import Consulta from './components/Consulta'
 function App() {
   return (
     <div className="App">
+      <h1>:: Consulta CEP ::</h1>
+
       <div className="header">
         <a href="https://github.com/thejohnpg/consulta-cep-reactjs/" target="_blank" className="header-icon"><FaGithub /></a> 
         <a href="https://www.linkedin.com/in/thejohnpg/" target="_blank" className="header-icon" ><FaLinkedin /></a> 
       </div>
+
      <Consulta />
     </div>
   );
